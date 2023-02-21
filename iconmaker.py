355779a884eebb6e7d1dd86017d59ddd75e738f6
Chris <christopherbloodsworth@gmail.com>
2023-02-21 11:10:12 -0500

    Added iconmaker (similar to github style)

--- iconmaker.py
+++ iconmaker.py
@@ -2,18 +2,23 @@ import random
 import matplotlib.pyplot as plt
 import numpy as np
 
+SIZE = 5
+
+# We don't want even-numbered sizes
+if SIZE % 2 == 0: SIZE += 1
+
 pixelmap = []
 color = np.array([random.randint(0,255),random.randint(0,255),random.randint(0,255)])
 
-for i in range(5):
+for i in range(SIZE):
     pixelmap.append([])
-    for j in range(3):
+    for j in range(SIZE//2 + 1):
         pixelmap[i].append(color * random.randint(0,1))
+
+    for j in range(SIZE//2-1,-1,-1):
+        pixelmap[i].append(pixelmap[i][j])
         
 
-for i in range(5):
-    for j in range(1,-1,-1):
-        pixelmap[i].append(pixelmap[i][j])
 
 
 plt.figure()
